6698937db4daab32101f3de2d27cac70b56f8639
Devanshu Jain <jaindevanshu236@gmail.com>
2024-03-02 04:05:49 +0530

    Logos Error bug 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ export const Header = () => {
   return (
     <div className={styles.navbar}>
       <div className={styles.left}>
-        <img src="../../public/user.png" alt="Image" width={50} />
+        <img src="../../user.png" alt="Image" width={50} />
         <span>Contact Manager</span>
       </div>
       <div className={styles.right}>
         <a href="http://dj-gpt-sigma.vercel.app">
           <span>Ai Assistant</span>
-          <img src="../../public/chat-bot.png" alt="Image" width={50} />
+          <img src="../../chat-bot.png" alt="Image" width={50} />
         </a>
       </div>
     </div>
